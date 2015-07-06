8b0f11658ff7d7bab13363444de6a8112686ff04
Mandy Neumann <mandy@aphrodite.spinfo.uni-koeln.de>
2015-07-06 15:52:07 +0200

    Implement dimensionality reduction

--- src/main/java/suffixTreeClustering/svd/DimensionReducer.java
+++ src/main/java/suffixTreeClustering/svd/DimensionReducer.java
@@ -1,25 +1,81 @@
 package suffixTreeClustering.svd;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import suffixTreeClustering.data.Type;
+import suffixTreeClustering.features.FeatureVector;
+import suffixTreeClustering.st_interface.SuffixTreeInfo;
+import util.ArrayConverter;
+
+import com.aliasi.matrix.SvdMatrix;
 
 public class DimensionReducer {
 
-	public static List<Type> reduce(Set<Type> types) {
-		List<Type> toReturn = new ArrayList<Type>(types);
+	public static List<Type> reduce(SuffixTreeInfo corpus) {
+		List<Type> toReturn = new ArrayList<Type>(corpus.getTypes());
+
+		double[][] matrix = createMatrix(corpus);
+
+		double[][] docVectors = svd(matrix, toReturn);
+
+		for (int i = 0; i < docVectors.length; i++) {
+			double[] typeVector = docVectors[i];
+			toReturn.get(i).setFeatureVector(new FeatureVector(ArrayConverter.fromPrimitive(typeVector)));
+			/*
+			 * so einfach kann das eigentlich nicht sein, man muss ja sehen,
+			 * dass die ursprünglichen Types die richtigen Vektoren zugewiesen
+			 * bekommen. Das wird aber schwierig, weil die Types ursprünglich
+			 * ein Set sind. Lässt sich das noch ändern?
+			 */
+		}
 
 		/*
-		 * TODOs:
-		 * * create matrix from type vectors
-		 * * give matrix zu svd algorithm
-		 * * get back the term vectors
-		 * * set the type's vectors
+		 * TODOs: * create matrix from type vectors * give matrix zu svd
+		 * algorithm * get back the term vectors * set the type's vectors
 		 */
-		
+
 		return toReturn;
+	}
+
+	private static double[][] svd(double[][] matrix, List<Type> types) {
+		// TODO: determine factor k
+
+		int maxFactors = 200; // number of factors = latent semantic dimensions
+		double featureInit = 0.01;
+		double initialLearningRate = 0.005;
+		int annealingRate = 1000;
+		double regularization = 0.00;
+		double minImprovement = 0.0000;
+		int minEpochs = 10;
+		int maxEpochs = 50000;
+
+		SvdMatrix svd = SvdMatrix.svd(matrix, maxFactors, featureInit,
+				initialLearningRate, annealingRate, regularization, null,
+				minImprovement, minEpochs, maxEpochs);
+
+		double[] scales = svd.singularValues(); // =Sigma
+		double[][] termVectors = svd.leftSingularVectors(); // =U
+		double[][] docVectors = svd.rightSingularVectors(); // =V
+
+		return termVectors;
+	}
+
+	private static double[][] createMatrix(SuffixTreeInfo corpus) {
+		double[][] matrix = new double[corpus.getNumberOfTypes()][corpus
+				.getNumberOfNodes()];
+
+		int row = 0;
+		for (Type type : corpus.getTypes()) {
+			FeatureVector vector = type.getVector();
+			double[] values = ArrayUtils.toPrimitive(vector.getValues());
+			matrix[row] = values;
+
+		}
+		return matrix;
 	}
 
 }
